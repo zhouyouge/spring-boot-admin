298278f416e2020120a4a15835f761074f2112f2
zhouyouge <57120517+zhouyouge@users.noreply.github.com>
2020-12-30 16:24:42 +0800

    admin-client为客户端实例2

---
+++ admin-client2/src/main/java/com/adminclient2/AdminClient2Application.java
@@ -0,0 +1,13 @@
+package com.adminclient2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminClient2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminClient2Application.class, args);
+    }
+
+}
---
+++ admin-client2/src/test/java/com/adminclient2/AdminClient2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adminclient2;
+
+/*import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminClient2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}*/
